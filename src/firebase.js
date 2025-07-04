6484e556bc6cdfeedc0183a5b4f67dec7a7d0c5c
Syly <nlo.yafg@gmail.com>
2025-07-04 23:40:34 +0300

    переніс ключі

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAlXzcIqaZ-ewRwTJZCwwjIOsPxR3gXPmQ",
-  authDomain: "my-diary-a3900.firebaseapp.com",
-  projectId: "my-diary-a3900",
-  storageBucket: "my-diary-a3900.firebasestorage.app",
-  messagingSenderId: "329986863549",
-  appId: "1:329986863549:web:d9f3fe4e1a9057ce0022d4",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export the initialized app
 export const auth = getAuth(app);
 export const db = getFirestore(app);
